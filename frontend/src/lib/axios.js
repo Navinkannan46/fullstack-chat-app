6732944049192834aab6b9309e4f84aad691d9ef
Navinkannan46 <navinkannan46@gmail.com>
2025-10-29 20:18:25 +0530

    second commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"        // dev on host
-      : "http://backend:5000/api",         // prod in Docker
+      ? "http://localhost:5000/api"
+      : "/api",
   withCredentials: true,
 });
